f64e71d0df01c84e2202effd06ef12c806e0d54f
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-27 19:26:33 -0400

    GameRules content added

--- src/GameRules.jsx
+++ src/GameRules.jsx
@@ -9,6 +9,12 @@ function GameRules() {
             <h2>OBJECTIVE</h2>
               <p>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).</p>
             <h2>HOW TO PLAY</h2>
+              <ol>
+                <li>Red goes first in the first game.</li>
+                <li>Players must alternate turns, and only one disc can be dropped in each turn.</li>
+                <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
+                <li>The starter of the previous game goes second on the next game.</li>
+              </ol>
         </div>
     </section>
   )
