7613e9980e01862717d0c6011c4df30d6349fe01
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-28 09:18:56 -0400

    Mobile game rules page complete

--- src/GameRules.jsx
+++ src/GameRules.jsx
@@ -1,4 +1,6 @@
 import styles from './GameRules.module.css'
+import check from './assets/images/icon-check.svg'
+import { Link } from 'react-router-dom';
 
 function GameRules() {
 
@@ -9,12 +11,15 @@ function GameRules() {
             <h2 className={styles[`rules-subheading`]}>OBJECTIVE</h2>
               <p className={styles[`rules-text`]}>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).</p>
             <h2 className={styles[`rules-subheading`]}>HOW TO PLAY</h2>
-              <ol>
+              <ol className={styles[`rules-list`]}>
                 <li>Red goes first in the first game.</li>
                 <li>Players must alternate turns, and only one disc can be dropped in each turn.</li>
                 <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
                 <li>The starter of the previous game goes second on the next game.</li>
               </ol>
+            <Link to="/">
+              <img src={check} className={styles['check-icon']}/>
+            </Link>
         </div>
     </section>
   )
