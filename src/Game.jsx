6a78921ac1399b908f71600d04509e7245fadca7
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-29 16:58:06 -0400

    Game Component buttons added and styled

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,13 +1,21 @@
 import logo from './assets/images/logo.svg'
-import styles from './MainMenu.module.css'
+import board from './assets/images/board-layer-white-small.svg'
+import styles from './Game.module.css'
 
-function MainMenu() {
+function Game() {
 
   return (
-    <div>
+    <div className={styles['game-container']}>
+      <div className={styles['game-nav']}>
+        <button className={styles['game-button']}>MENU</button>
         <img className={styles['logo']} src={logo} />
+        <button className={styles['game-button']}>RESTART</button>
+      </div>
+      <div className={styles['game-scores-container']}>
+      </div>
+      <img className={styles['game-board']} src={board} />
     </div>
   )
 }
 
-export default MainMenu
+export default Game
